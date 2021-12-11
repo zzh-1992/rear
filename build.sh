dee77d128dafeb8ef2f0d9adc29c3ff2617131fd
zzh-1992 <ezzh1992@icloud.com>
2021-12-11 14:42:46 +0800

    完善部署脚本

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ echo "=========Begin to create docker image========="
 echo "=============================================="
 
 # 删除原有镜像
-docker rm rear/rear:2.0
+docker rmi rear/rear:2.0
+
+# 停止原有容器
+docker stop rear2.0
 # 删除原有容器
-docker rm /rear:2.0
+docker rm rear2.0
 
 # 创建新镜像
 docker build -t rear/rear:2.0 .
@@ -18,5 +21,3 @@ docker run --name rear2.0 -d -p 8888:8080 rear/rear:2.0
 echo "=============================================="
 echo "============success to run server!============"
 echo "=============================================="
-
-
