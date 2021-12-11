a4c1235b19036f67509c9e2ed1e7e4c5e691ddd2
zzh-1992 <ezzh1992@icloud.com>
2021-12-11 17:21:54 +0800

    同步容器端口8888

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ docker rm rear2.0
 docker build -t rear/rear:2.0 .
 
 # 使用镜像创建容器
-docker run --name rear2.0 -d -p 8888:8080 rear/rear:2.0
+docker run --name rear2.0 -d -p 8888:8888 rear/rear:2.0
 
 echo "=============================================="
 echo "============success to run server!============"
